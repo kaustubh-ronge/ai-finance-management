6d5c922e46915347b4e631652bf392e312570473
kaustubh-ronge <kaustubhronge25@gmail.com>
2025-10-01 10:49:19 +0530

    updated

--- actions/transaction.js
+++ actions/transaction.js
@@ -105,7 +105,7 @@ function calculateNextRecurringDate(startDate, interval) {
 
 export async function scanReceipt(file) {
   try {
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
     const arrayBuffer = await file.arrayBuffer();
 
